16e7bfe251a5e44b44594d6bb300a53dcd687642
arquinael.filho <arquinael.filho@unimednatal.com.br>
2023-01-20 14:11:37 -0300

    Ajustado para buscar o nome de usuario da sessao

--- src/containers/Sidebar/UserProfile.js
+++ src/containers/Sidebar/UserProfile.js
@@ -4,7 +4,7 @@ import {useAuth} from "../../authentication";
 import {useHistory} from "react-router-dom";
 
 const UserProfile = () => {
-  const {userSignOut} = useAuth();
+  const {userSignOut, authUser} = useAuth();
   const history = useHistory();
 
   const onLogoutClick = () => {
@@ -25,8 +25,11 @@ const UserProfile = () => {
   return (
     <div className="gx-flex-row gx-align-items-center gx-mb-4 gx-avatar-row">
       <Popover placement="bottomRight" content={userMenuOptions} trigger="click">
-        <Avatar src={"https://via.placeholder.com/150"} className="gx-size-40 gx-pointer gx-mr-3" alt=""/>
-        <span className="gx-avatar-name">Rob Farnandies<i className="icon icon-chevron-down gx-fs-xxs gx-ml-2"/></span>
+        {authUser && authUser.photo != null ? <Avatar src={"https://via.placeholder.com/150"} className="gx-size-40 gx-pointer gx-mr-3" alt=""/> : ''}
+        <span className="gx-avatar-name">
+          {authUser ? authUser.name : "Carregando..."}
+          <i className="icon icon-chevron-down gx-fs-xxs gx-ml-2"/>
+        </span>
       </Popover>
     </div>
   )
