8f36bf056e4956626c7b975075bd815c967da83a
arquinael.filho <arquinael.filho@unimednatal.com.br>
2023-01-26 16:54:43 -0300

    Add rota principal

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,6 +18,7 @@ const App = ({match}) => (
 			<Route path={`${match.url}origens`} component={asyncComponent(() => import('./Origens'))}/>
 			<Route path={`${match.url}destinos`} component={asyncComponent(() => import('./Destinos'))}/>
 			<Route path={`${match.url}contratos`} component={asyncComponent(() => import('./Contratos'))}/>
+			<Route path={`${match.url}assuntos`} component={asyncComponent(() => import('./Assuntos'))}/>
 		</Switch>
 	</div>
 );
