ecc72e655aa2f718aa53e05c8160a72195173a82
arquinael.filho <arquinael.filho@unimednatal.com.br>
2023-02-15 17:33:35 -0300

    Add rota para responsaveis

--- src/routes/CentrosCusto/index.js
+++ src/routes/CentrosCusto/index.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom"
 import List from "./List"
 import Add from "./Add"
 import Edit from "./Edit"
+import Responsaveis from "./Responsaveis"
 
 const controller = "ccustos"
 const CentrosCusto = () => {
@@ -13,6 +14,7 @@ const CentrosCusto = () => {
                 <Route exact path={`/${controller}`} component={(props) => <List {...props} controller={controller} />} />
                 <Route exact path={`/${controller}/adicionar`} component={(props) => <Add {...props} controller={controller} />} />
                 <Route exact path={`/${controller}/editar/:id`} component={(props) => <Edit {...props} controller={controller} />} />
+                <Route exact path={`/${controller}/responsaveis/:id`} component={(props) => <Responsaveis {...props} controller={controller} />} />
             </Switch>
         </div>
     )
