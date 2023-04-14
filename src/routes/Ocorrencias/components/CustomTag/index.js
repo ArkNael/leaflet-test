be0120b51b8b763f22ae38cb14bea5eb19e11f21
arquinael.filho <arquinael.filho@unimednatal.com.br>
2023-04-14 09:52:11 -0300

    Ajustes para as situacoes das ocorrencias

--- src/routes/Ocorrencias/components/CustomTag/index.js
+++ src/routes/Ocorrencias/components/CustomTag/index.js
@@ -5,21 +5,16 @@ const CustomTag = ({children, textOnly=false, ...rest}) => {
 	let color, icon = ''
 
 	switch (children) {
-		case 'Primeira Iteração':		color = '';			   icon = <Icons.RightOutlined />;     								   	 break;
-		case 'Ocorrencia Criada':		color = '';			   icon = <Icons.RightOutlined />;     								   	 break;
-		case 'Encaminhamento':			color = 'blue';      icon = <Icons.ArrowRightOutlined />;   children = 'Encaminhado';	  	break;
-		case 'Ocorrencia Encaminhada':	color = 'blue';      icon = <Icons.ArrowRightOutlined />;   children = 'Encaminhado';	  	break;
-		case 'Solicitação de Pausa':	color = 'volcano';	   icon = <Icons.ClockCircleOutlined />;  children = 'Pausa Solicitada'; break;
-		case 'Solicitacao de Pausa':	color = 'volcano';	   icon = <Icons.ClockCircleOutlined />;  children = 'Pausa Solicitada'; break;
-		case 'Pausa concedida':			color = 'cyan';      icon = <Icons.ClockCircleOutlined />;  								break;
-		case 'Pausa Concedida':			color = 'cyan';      icon = <Icons.ClockCircleOutlined />;  								break;
+		case 'Ocorrencia Criada':		color = '';			   icon = <Icons.RightOutlined />;     	  children = 'Iniciado';	   	 break
+		case 'Ocorrencia Encaminhada':	color = 'blue';      icon = <Icons.ArrowRightOutlined />;   children = 'Encaminhado';	  	break
+		case 'Solicitacao de Pausa':	color = 'volcano';	   icon = <Icons.ClockCircleOutlined />;  children = 'Pausa Solicitada'; break
+		case 'Pausa Concedida':			color = 'cyan';      icon = <Icons.ClockCircleOutlined />;  								break
 		case 'Pausa Negada':			color = 'red';       icon = <Icons.StopOutlined />;  								  	  	break
-		case 'Resposta':				color = 'green';     icon = <Icons.ArrowLeftOutlined />;    								break;
-		case 'Ocorrencia Respondida':	color = 'green';     icon = <Icons.ArrowLeftOutlined />;    children = 'Respondido';		break;
-		case 'Última Iteração':			color = '';			   icon = <Icons.CheckOutlined />;									   	 break;
-		case 'Ocorrencia Finalizada':	color = '';			   icon = <Icons.CheckOutlined />;		  children = 'Finalizado';   	 break;
+		case 'Ocorrencia Respondida':	color = 'green';     icon = <Icons.ArrowLeftOutlined />;    children = 'Respondido';		break
+		case 'Ocorrencia Finalizada':	color = '';			   icon = <Icons.CheckOutlined />;		  children = 'Finalizado';   	 break
+		case 'Ocorrencia Escalada':		color = 'purple';	  icon = <Icons.ArrowUpOutlined />;		 children = 'Escalado';   	 	break
 
-		default: break;
+		default: break
 	}
 
 	return (
--- src/routes/Ocorrencias/components/IconeMovimentacao/index.js
+++ src/routes/Ocorrencias/components/IconeMovimentacao/index.js
@@ -2,15 +2,12 @@ import * as Icons from '@ant-design/icons'
 
 const getIconeMovimentacao = tipo => {
 	switch (tipo) {
-		case 'Última Iteração': 		return {dot: <Icons.CheckOutlined style={{fontSize: 15}}/>}
 		case 'Ocorrencia Finalizada': 	return {dot: <Icons.CheckOutlined style={{fontSize: 15}}/>}
-		case 'Encaminhamento': 			return {color: '#096dd9'}
 		case 'Ocorrencia Encaminhada': 	return {color: '#096dd9'}
 		case 'Pausa Negada':			return {dot: <Icons.StopOutlined style={{fontSize: 15}} />, color: '#cf1322'}
-		case 'Pausa concedida':			return {dot: <Icons.ClockCircleOutlined style={{fontSize: 15}} />, color: '#00aeff'}
 		case 'Pausa Concedida':			return {dot: <Icons.ClockCircleOutlined style={{fontSize: 15}} />, color: '#00aeff'}
-		case 'Solicitação de Pausa':    return {dot: <Icons.ClockCircleOutlined style={{fontSize: 15}} />, color: '#ffbb00'}
 		case 'Solicitacao de Pausa':    return {dot: <Icons.ClockCircleOutlined style={{fontSize: 15}} />, color: '#ffbb00'}
+		case 'Ocorrencia Escalada':     return {dot: <Icons.ArrowUpOutlined style={{fontSize: 15}} />, color: '#531dab'}
 		default: 						return null;
 	}
 }
